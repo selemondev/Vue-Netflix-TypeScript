6625d74e94f714d9ba3c434a8da8faa081d5205d
selemondev <selemondev19@gmail.com>
2022-07-28 21:49:49 +0300

    :zap: Youtube Component

---
+++ src/Requests/requests.ts
@@ -0,0 +1,12 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie/api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movies/api_key=${API_KEY}&with_genres=99`,
+}
+export default requests;
---
+++ src/Types/movieDataTypes.ts
@@ -0,0 +1,31 @@
+export interface MovieTypes {
+    adult:             boolean;
+    backdrop_path:     string;
+    id:                number;
+    title?:            string;
+    original_language: OriginalLanguage;
+    original_title?:   string;
+    overview:          string;
+    poster_path:       string;
+    media_type:        MediaType;
+    genre_ids:         number[];
+    popularity:        number;
+    release_date?:     string;
+    video?:            boolean;
+    vote_average:      number;
+    vote_count:        number;
+    name?:             string;
+    original_name?:    string;
+    first_air_date?:   string;
+    origin_country?:   string[];
+}
+
+export enum MediaType {
+    Movie = "movie",
+    Tv = "tv",
+}
+
+export enum OriginalLanguage {
+    En = "en",
+    Ko = "ko",
+}
